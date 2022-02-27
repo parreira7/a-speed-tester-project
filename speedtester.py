170eaf8c6b7d5f468e1a501ebb2e944b51fdbff2
Gabriel <99111916+parreira7@users.noreply.github.com>
2022-02-27 17:18:12 -0300

    Update speedtester.py

--- speedtester.py
+++ speedtester.py
@@ -1,3 +1,4 @@
+from distutils import command
 from tkinter import *
 from PIL import Image, ImageTk
 
@@ -15,34 +16,26 @@ barra = ('#1e81b0')
 wdw = Tk ()
 wdw.title ('a speed tester by g!')
 wdw.geometry("500x300")
-wdw.configure(background=c1)
+wdw.configure(background=marrom1)
 wdw.resizable(width=FALSE, height=FALSE)
-
-#stripe at mid-screen
-frame_logo = Frame(wdw, width=500, height=250, bg=c1)
-frame_logo.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
-
-frame_stripe = Frame(wdw, width=250, height=5, bg=cS)
-frame_stripe.grid(row=1, column=0, pady=0, padx=0, sticky=NSEW)
-frame_stripe = Frame(wdw, width=250, height=5, bg=cL)
-frame_stripe.grid(row=2, column=0, pady=0, padx=0, sticky=NSEW)
+icon=PhotoImage('ico.ico')
+wdw.iconbitmap(True,icon)
 
 #img
-img = Image.open('net.png')
+img = Image.open('ico.ico')
 img = img.resize((72,72))
 img = ImageTk.PhotoImage(img)
 
 #wifi logo
-l_logo_img = Label(frame_logo, height=500, image=img, compound=LEFT, padx=10, anchor='nw', bg=c1, fg= c1)
-l_logo_img.place(x=25, y=0)
+l_logo_img = Label(wdw, height=500, image=img, compound=LEFT, padx=10, anchor='nw', bg=marrom1, fg= marrom1)
+l_logo_img.place(x=210, y=5)
 
 #text under logo
-l_logo_name = Label(frame_logo,text='a Net Tester!',font=('BebasNeue'), padx=10, anchor='ne', bg=c1, fg= cB)
-l_logo_name.place(x=17.5, y=70)
+l_logo_name = Label(wdw,text='a Net Tester!',font=('NTWagner 16'), padx=10, anchor='ne', bg=marrom1, fg=marromF)
+l_logo_name.place(x=180, y=85)
 
 #function
 import speedtest
-
 speed = speedtest.Speedtest()
 def main():
 
@@ -55,29 +48,28 @@ def main():
     l_upload ['text'] = upload
     
 #test button
-    button_test['text'] = 'Test again'
-    button_test.place(x=215, y=215)
+    button_test['text'] = 'TEST'
+    button_test.place(x=220, y=260)
 
     
-
 #cfg example>
-l_download = Label(frame_logo, text=('50'), padx=10, anchor='ne',font=('Arial 28'), bg=c1, fg= cB)
-l_download.place(x=330, y=150)
-l_mbps = Label(frame_logo, text=('Mbps/s Down.'), padx=10, anchor='ne',font=('BebasNeue'), bg=c1, fg= cB)
-l_mbps.place(x=330, y=200)
+l_download = Label(wdw, text=('xx'), padx=10, anchor='ne',font=('NTWagner 20'), bg=marrom1, fg= marromF)
+l_download.place(x=350, y=150)
+l_mbps = Label(wdw, text=('Mbp/s Down.'), padx=10, anchor='ne',font=('NTWagner 14'), bg=marrom1, fg= marromF)
+l_mbps.place(x=335, y=200)
 
 #cfg example<
-l_upload = Label(frame_logo, text=50, padx=10, anchor='ne',font=('Arial 28'), bg=c1, fg= cB)
+l_upload = Label(wdw, text=('xx'), padx=10, anchor='ne',font=('NTWagner 20'), bg=marrom1, fg= marromF)
 l_upload.place(x=75, y=150)
-l_mpbs = Label(frame_logo, text=('Mbps/s Uplo.'), padx=10, anchor='ne',font=('BebasNeue'), bg=c1, fg= cB)
-l_mpbs.place(x=75, y=200)
+l_mpbs = Label(wdw, text=('Mbp/s Uplo.'), padx=10, anchor='ne',font=('NTWagner 14'), bg=marrom1, fg= marromF)
+l_mpbs.place(x=50, y=200)
 
 #img up
 imgup = Image.open('up.png')
 imgup = imgup.resize((50,50))
 imgup = ImageTk.PhotoImage(imgup)
 #up logo
-l_logo_imgup = Label(frame_logo, height=500, image=imgup, compound=LEFT, padx=10, anchor='ne', bg=c1, fg=c1)
+l_logo_imgup = Label(wdw, height=500, image=imgup, compound=LEFT, padx=10, anchor='ne', bg=marrom1, fg=marrom1)
 l_logo_imgup.place(x=185, y=150)
 
 #img down
@@ -85,12 +77,11 @@ imgdown = Image.open('down.png')
 imgdown = imgdown.resize((50,50))
 imgdown = ImageTk.PhotoImage(imgdown)
 #down logo
-l_logo_imgdown = Label(frame_logo, height=500, image=imgdown, compound=LEFT, padx=10, anchor='ne', bg=c1, fg=c1)
+l_logo_imgdown = Label(wdw, height=500, image=imgdown, compound=LEFT, padx=10, anchor='ne', bg=marrom1, fg=marrom1)
 l_logo_imgdown.place(x=260, y=150)
 
 #placing test button
-button_test = Button(frame_logo,command=main, height=25, text=('Start Test'), font=('BebasNeue'), relief=RAISED, overrelief=RIDGE, compound=LEFT, padx=1, anchor='nw', bg=cS, fg=cL)
-button_test.place(x=215, y=215)
-
+button_test = Button(wdw, command=main, height=10, text=('START'), font=('NTWagner 10'), relief=GROOVE, overrelief=GROOVE, compound=CENTER, padx=0, anchor='nw', bg=marrom1, fg=marromF)
+button_test.place(x=220, y=260)
 
 wdw.mainloop()
